8a17635a8fcaeb772f7c8036286fa4434a8ed8d1
glebite <glebite@gmail.com>
2021-03-06 09:32:01 -0500

    fleshing out client

--- src/client.py
+++ src/client.py
@@ -9,15 +9,54 @@ threading, timers, connections, reconnections and state management.
 """
 
 import sys
+import getopt
+
+
+class Client:
+    """
+    """
+    def __init__(self, hostip, port):
+        self.hostip = hostip
+        self.port = port
+
+    def run(self):
+        """
+        """
+        pass
+
+
+def usage():
+    r"""Prints usage to stdout
+
+    :param:  None
+    :return: None
+    """
+    print('Usage: ')
+    sys.exit(1)
 
 
 def main(arguments):
     r"""Handles the processing of arguments and running the client.
 
-    :param arguments: command line arguments from sys.argv
+    -s --server= ip address of the server to connect to
+    -p --port=   port for the server
+
+    :param:  arguments: command line arguments from sys.argv
     :return: None
     """
-
+    options, _ = getopt.getopt(arguments,
+                               'hs:p:', ['server=', 'port='])
+    for opt, arg in options:
+        if opt in ('-s', '--server'):
+            server = arg
+        elif opt in ('-p', '--port'):
+            port = int(arg)
+        elif opt in ('-h'):
+            usage()
+        else:
+            usage()
+    client = Client(server, port)
+    client.run()
     return None
 
 
