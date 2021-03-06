2f066cf14643d9c04d34b09eab06f7153d4d801e
glebite <glebite@gmail.com>
2021-03-06 07:28:13 -0500

    Server template is created

---
+++ src/server.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+server.py
+---------
+
+This module implements code for learning about server connections,
+threading, timers, connections, reconnections and state management.
+"""
+
+import sys
+
+
+def main(arguments):
+    r"""Handles the processing of arguments and running the server.
+
+    :param arguments: command line arguments from sys.argv
+    :return: None
+    """
+
+    return None
+
+
+if __name__ == "__main__":
+    main(sys.argv)
