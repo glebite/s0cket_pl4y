bf604d1466dbd0353981fcabd8291300907b3cff
glebite <glebite@gmail.com>
2021-03-06 07:30:28 -0500

    Pushing in client template code.

---
+++ src/client.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+client.py
+---------
+
+This module implements code for learning about client connections,
+threading, timers, connections, reconnections and state management.
+"""
+
+import sys
+
+
+def main(arguments):
+    r"""Handles the processing of arguments and running the client.
+
+    :param arguments: command line arguments from sys.argv
+    :return: None
+    """
+
+    return None
+
+
+if __name__ == "__main__":
+    main(sys.argv)
